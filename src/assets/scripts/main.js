2ba71a654d4b0c8ec2473aa8c6c858c7fd864f7c
EnricMS <enricmiralles96@gmail.com>
2023-12-03 17:51:18 +0100

    js + inici pseudoclasses (falta fer be)
    Tambe adaptació de @supports

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,3 +14,27 @@
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var form = document.querySelector('.general__sec__form');
+
+  form.addEventListener('submit', function (e) {
+    var isValid = true;
+    var inputs = form.querySelectorAll('input[required], textarea[required]');
+
+    inputs.forEach(function (input) {
+      if (!input.value) {
+        isValid = false;
+        input.classList.add('is-invalid'); 
+      } else {
+        input.classList.remove('is-invalid');
+      }
+    });
+
+    if (!isValid) {
+      e.preventDefault(); 
+      alert('Si us plau, ompli tots els camps requerits.');
+    }
+  });
+});
